959b8f4d6dc482382523e109416bdca3d628c2d2
Finn <contact@bechinka.eu>
2022-05-13 00:25:18 +0200

    implemented canny edge detection

--- p4/main.py
+++ p4/main.py
@@ -1,4 +1,6 @@
 import cv2
+from cv2 import filter2D
+from cv2 import imwrite
 import numpy as np
 import math
 
@@ -10,7 +12,13 @@ def display(img, name="image", wait=False):
         cv2.destroyAllWindows()
 
 
+# takes in n = matrix size (must be odd or n = 1 will be used by default)
+# and sigma for the calculation of the gaussian filter
+# returns gaussian filter kernel of size (2 * n + 1)x(2 * n + 1)
 def calc_gaussian(n, sigma):
+    if n % 2 == 0:
+        n = 1
+
     matrix = []
 
     # distance between middle of the matrix and the edge
@@ -33,6 +41,139 @@ def calc_gaussian(n, sigma):
     return np.array(matrix)
 
 
+# takes in an image and gaussian filter kernel
+# returns new blurred image
+def gaussian(img, kernel=calc_gaussian(1, 5.5)):
+    return cv2.filter2D(img, -1, kernel)
+
+
+# takes grayscale image
+# returns gradient magnitute and direction
+def sobel(input):
+    # sobel filter matrices
+    S_X = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    S_Y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+
+    # apply sobel x and y filter to input image
+    resultx = cv2.filter2D(input, cv2.CV_32F, S_X)
+    resulty = cv2.filter2D(input, cv2.CV_32F, S_Y)
+
+    # calculate gradient magnitute
+    magnitute = np.hypot(resultx, resulty)  # Equivalent to sqrt(x1**2 + x2**2)
+    magnitute = magnitute / magnitute.max() * 255  # normalize values
+    magnitute = np.uint8(magnitute)  # convert to 8 bit
+
+    # calcutae gradient direction
+    direction = np.arctan2(resulty, resultx)
+
+    return (magnitute, direction)
+
+
+# takes in gradient magnitute and direction
+# returns non maxima suppression result
+def non_maxima_suppression(mag, dir):
+    rows, cols = mag.shape
+    result = np.zeros((rows, cols), np.uint8)
+    deg = np.rad2deg(dir)  # convert gadient direction to degrees
+
+    # iterate through gradient magnitute
+    # find neighbor a and b
+    for row in range(1, rows - 1):
+        for col in range(1, cols - 1):
+            a = 0
+            b = 0
+
+            # horizontal
+            if (
+                (0 <= deg[row, col] < 22.5)
+                or (157.5 <= deg[row, col] <= 180)
+                or (-22.5 <= deg[row, col] < 0)
+                or (-180 <= deg[row, col] < -157.5)
+            ):
+                a = mag[row, col + 1]
+                b = mag[row, col - 1]
+            # diagonal 45
+            elif (22.5 <= deg[row, col] < 67.5) or (-157.5 <= deg[row, col] < -112.5):
+                a = mag[row + 1, col + 1]
+                b = mag[row - 1, col - 1]
+            # vertical
+            elif (67.5 <= deg[row, col] < 112.5) or (-112.5 <= deg[row, col] < -67.5):
+                a = mag[row + 1, col]
+                b = mag[row - 1, col]
+            # diogonal 135
+            elif (112.5 <= deg[row, col] < 157.5) or (-67.5 <= deg[row, col] < -22.5):
+                a = mag[row + 1, col - 1]
+                b = mag[row - 1, col + 1]
+
+            # check if local maxima is bigger than neighbors
+            if (mag[row, col] > a) and (mag[row, col] > b):
+                result[row, col] = mag[row, col]
+            else:
+                result[row, col] = 0
+
+    return result
+
+
+# takes gradient magnitute, low and high threshold
+# returns gradient magnitute with thinned out edges
+def hysteresis(mag, t_low, t_high):
+    rows, cols = mag.shape
+    result = np.zeros((rows, cols), np.uint8)
+
+    # iterate thorugh image and set values acording to threshold values
+    for row in range(0, rows):
+        for col in range(0, cols):
+            if mag[row, col] > t_high:
+                result[row, col] = 255
+            if mag[row, col] < t_low:
+                result[row, col] = 0
+            if (mag[row, col] >= t_low) & (mag[row, col] <= t_high):
+                result[row, col] = 128
+
+    # check for all weak edges if they are adjacent to a strong edge
+    # if a weak edge is adjacent to a strong edge elevate it (set value to 255)
+    # otherwise suppress it (set value to 0)
+    for row in range(1, rows - 1):
+        for col in range(1, cols - 1):
+            if result[row, col] == 128:
+                connected = False
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        if result[row + i, col + j] == 255:
+                            connected = True
+                if connected:
+                    result[row, col] = 255
+                else:
+                    result[row, col] = 0
+
+    return result
+
+
 img = cv2.imread("assets/p04_apfelbaum.png", cv2.IMREAD_GRAYSCALE)
 
-display(cv2.filter2D(img, -1, calc_gaussian(1, 5.5)), "blurred", True)
+# (a)
+gauss_kernel = calc_gaussian(1, 5.5)
+blurred_img = gaussian(img, gauss_kernel)
+
+# (b)
+sobel_mag, sobel_dir = sobel(blurred_img)
+
+# (c)
+nms_img = non_maxima_suppression(sobel_mag, sobel_dir)
+
+# (d)
+canny = hysteresis(nms_img, 25, 50)
+
+EXPORT_IMAGES = True  # sets if images should be saved to file (into assets/export/)
+SHOW_IMAGES = True  # sets if images should be shown in windows
+
+if EXPORT_IMAGES:
+    cv2.imwrite("assets/export/blurred_img.png", blurred_img)
+    cv2.imwrite("assets/export/sobel_mag.png", sobel_mag)
+    cv2.imwrite("assets/export/nms_img.png", nms_img)
+    cv2.imwrite("assets/export/canny.png", canny)
+if SHOW_IMAGES:
+    display(blurred_img, "gaussian blur")
+    display(sobel_mag, "sobel filter")
+    display(nms_img, "nms")
+    display(canny, "canny", True)
